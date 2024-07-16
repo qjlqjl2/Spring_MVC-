8ecf9d785e67f5aabe6dade78bd8aa4260b511b2
QJL <2350235142@qq.com>
2024-07-16 11:01:25 +0800

    第一个版本

---
+++ src/main/java/com/powernode/usermgt/bean/User.java
@@ -0,0 +1,60 @@
+package com.powernode.usermgt.bean;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private Integer gender;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, Integer gender) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
